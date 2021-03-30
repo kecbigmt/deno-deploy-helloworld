9bb536af9d36c330b752e72de39b1cfd5ba6f6b5
Keishiro Oyama <kecy@KeishironoMacBook-Pro.local>
2021-03-30 14:54:06 +0900

    タイムゾーンを修正

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 addEventListener('fetch', (event: FetchEvent) => {
-  const dt = new Date().toLocaleString('ja-JP');
+  const dt = new Date().toLocaleString('ja-JP', { timeZone: 'JST' });
   const response = new Response(`<html><p><< ただいまの時刻は${dt} >> 🐓</p><p>GitHub: <a href="https://github.com/kecbigmt/deno-deploy-helloworld">https://github.com/kecbigmt/deno-deploy-helloworld</a></p></html>`, {
     headers: { 'content-type': 'text/html;charset=UTF-8' },
   });
