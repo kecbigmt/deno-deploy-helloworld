b9a141a8130f54c3628957fb47dd2d446d6c3995
Keishiro Oyama <kecy@KeishironoMacBook-Pro.local>
2021-03-30 14:38:09 +0900

    内容を時報に変更

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 addEventListener('fetch', (event: FetchEvent) => {
-  const response = new Response('Hello World', {
-    headers: { 'content-type': 'text/plain' },
+  const dt = new Date().toLocaleString('ja-JP');
+  const response = new Response(`ただいまの時刻は${dt}。こちらのソースコードでお届けしています👉 https://github.com/kecbigmt/deno-deploy-helloworld`, {
+    headers: { 'content-type': 'text/plain;charset=UTF-8' },
   });
   event.respondWith(response);
 });
